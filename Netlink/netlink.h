f279fdcbbb7240aa68c86c66c2cc027d1757a1e1
Osqar1 <dudemaimon@gmail.com>
2020-05-19 21:51:00 +0300

    Added basic client code

---
+++ Netlink/netlink.h
@@ -0,0 +1,10 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netlink.h>
+#include <net/netlink.h>
+#include <net/net_namespace.h>
+
+void send_to_user(char* msg);
+int createNetlink(void);
+int releaseNetlink(void);
+

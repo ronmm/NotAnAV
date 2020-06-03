3854d08e7bb5d665b4ca04a5758f273065c353b8
Osqar1 <dudemaimon@gmail.com>
2020-06-03 18:36:26 +0300

    Improved hooks code
    1. Deleted mkdir hook
    2. execve and connect are now sending data to user space in the following format:
    <EXECVE/CONNECT>: key1=value1, key2=value2 ...

--- Hook/hooks.h
+++ Hook/hooks.h
@@ -7,15 +7,11 @@
 #include "types.h"
 #include "netlink.h"
 
-typedef asmlinkage int (*sys_mkdir_t)(const char *, int);
-typedef asmlinkage int (*sys_execve_t)(const char *, char *const [], char *const []);
+typedef asmlinkage int (*sys_execve_t)(const char *, const char *const *, const char *const * );
 typedef asmlinkage int (*sys_connect_t)(int, const struct sockaddr *, unsigned int);
 
 asmlinkage int
-mkdir_hook(const char *, int);
-
-asmlinkage int
-execve_hook(const char *, char *const [], char *const []);
+execve_hook(const char *, const char *const *, const char *const *);
 
 asmlinkage int
 connect_hook(int sockfd, const struct sockaddr *addr, unsigned int addrlen);

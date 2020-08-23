2acf201e9a9ed157363691766ca39230f9b5f2a2
Or Maimon <ormaimon117@gmail.com>
2020-08-23 16:41:15 +0300

    Added timestamp to CSV

--- data_logger.c
+++ data_logger.c
@@ -1,6 +1,16 @@
 #include "data_logger.h"
 #define CSV_FILE "/tmp/data.csv"
 
+void get_timestamp(char * timestamp)
+{
+	time_t current_time = time(NULL);
+	sprintf(timestamp, "\"time=%s\"", ctime(&current_time));
+	
+	// Remove newline from end of tiemstamp
+	timestamp[strlen(timestamp)-2] = '"';
+	timestamp[strlen(timestamp)-1] = '\0';
+}
+
 void log_kernel_data(char * data_from_kernel)
 {
 	FILE * csv_file = fopen(CSV_FILE, "a");
@@ -10,6 +20,10 @@ void log_kernel_data(char * data_from_kernel)
 		return;
 	}
 
-	fprintf(csv_file, "%s\n", data_from_kernel);
+	char timestamp[64] = {0};
+	get_timestamp(timestamp);
+        printf("data from kernel: '%s'\n", data_from_kernel);
+
+	fprintf(csv_file, "%s,%s\n", data_from_kernel, timestamp);
 	fclose(csv_file);
 }
